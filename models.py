bbb36ea6cf3b2037d4feba6c8500ea1abfb83e80
Dadarzz2405 <haidaralifawwaz@gmail.com>
2026-02-19 08:24:01 +0700

    fixed release ver

--- models.py
+++ models.py
@@ -1,9 +1,7 @@
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from datetime import datetime
 
-db = SQLAlchemy()
-
+from extensions import db
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
